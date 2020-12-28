f6d354ace8dc2e911c9b886d4bb7d97c6b92479a
enoyao <enoyao@tencent.com>
2020-12-28 13:18:59 +0800

    更新文档
    更新文档

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,8 @@ import * as vscode from 'vscode';
 import { generateBullShift } from './util';
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand("bullshift.generate", generateBullShift));
+	context.subscriptions.push(vscode.commands.registerCommand("bullshift.generate-bullshift", generateBullShift));
+	// context.subscriptions.push(vscode.commands.registerCommand("bullshift.fuck-javascript", fuckJavascript));
 }
 
 export function deactivate() { }
